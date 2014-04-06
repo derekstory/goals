27160e1eb71363f52e70c8e413110691ebda7e54
Derek Story <derek@ubuntu.(none)>
2014-04-06 04:28:36 -0400

    rated and not rated goals co-exist

--- goalLoad.php
+++ goalLoad.php
@@ -1,40 +1,48 @@
 <?php
 include'connect.php';
 
-$rate = mysql_query("SELECT *
-                        FROM `rating`, `users`
-			WHERE rating_author = user_name 
+$initial = mysql_query("SELECT *
+                        FROM `post`, `users`
+			WHERE post_author = user_name 
 			&& user_name = '" . $_SESSION['user_name'] . "'
-			&& DATE(rating_date) = DATE(NOW())");
-  $rateTotal = mysql_num_rows($rate);
+			");
+
+  $initialTotal = mysql_num_rows($initial);
   {
-    if($rateTotal < 1)
+    if($initialTotal < 1)
+    {
+    echo '<div>too bad yo</div>';
+    }
+    else
     {
 
     $post = mysql_query("SELECT *
-                        FROM `post`, `users`
-			WHERE post_author = user_name 
-			&& user_name = '" . $_SESSION['user_name'] . "'");
-        $postTotal = mysql_num_rows($post);
-        {
-        if($postTotal < 1)
-           {
-	   echo '<h4>Nothing is here yo</h4>';
-	   
-	   echo '<script>
-
-	   </script';
-	   }
+			 FROM post
+			 LEFT JOIN rating ON rating.rating_title = post_title
+			 AND post_author = '" . $_SESSION['user_name'] . "'
+			 AND DATE(rating_date) = DATE(NOW())
+			 ");
+        
+	   $postTotal = mysql_num_rows($post);
+          
+        
+           if($postTotal < 1)
+	   {
+	   }	   
 	   else
 	   while($postList = mysql_fetch_array($post))
 	   {
-           $postTitle = $postList["post_title"];	
-
+           $postTitle = $postList["post_title"];
+           $rateTitle = $postList["rating_title"];
+           $rateScore = $postList["rating_score"];
+           	
+           if($rateTitle == null)
+           {
            echo '<form>
    	     <div class="rateThis"> 
 	      <div class="rateThisWrap">
 	        <select>
-		  <option></option>
+		  <option class="selected"></option>
 		  <option>A</option>
 		  <option>A-</option>
 		  <option>B+</option>
@@ -51,41 +59,39 @@ $rate = mysql_query("SELECT *
 	        <h3>' . $postTitle . '</h3>
 	      </div>
 	     </div>
-           </form>';
-	   }
-	 }
-    }	 
-    else
-    while($rateList = mysql_fetch_array($rate))
-    {
-    $rateTitle = $rateList["rating_title"];	
-    $rateScore = $rateList["rating_score"];	
-    
-    echo '<form>
-      <div class="rateThis"> 
-      <div class="rateThisWrap">
-        <select>
-	  <option class="selected">' . $rateScore . '</option>
-	  <option value="100">A+</option>
-	  <option>A</option>
-	  <option>A-</option>
-	  <option>B+</option>
-	  <option>B</option>
-	  <option>B-</option>
-	  <option>C+</option>
-	  <option>C</option>
-	  <option>C-</option>
-	  <option>D+</option>
-	  <option>D</option>
-	  <option>D-</option>
-	  <option>F</option>
-        </select>
-        <h3>' . $rateTitle . '</h3>
-      </div>
-     </div>
-   </form>';
-  }
-}	
+            </form>';
+	    }
+            if(!empty($rateTitle))
+            {
+            echo '<form>
+            <div class="rateThis"> 
+            <div class="rateThisWrap">
+            <select>
+	     <option class="selected">' . $rateScore . '</option>
+	     <option value="100">A+</option>
+	     <option>A</option>
+	     <option>A-</option>
+	     <option>B+</option>
+	     <option>B</option>
+	     <option>B-</option>
+	     <option>C+</option>
+	     <option>C</option>
+	     <option>C-</option>
+	     <option>D+</option>
+	     <option>D</option>
+	     <option>D-</option>
+	     <option>F</option>
+            </select>
+            <h3>' . $rateTitle . '</h3>
+            </div>
+            </div>
+            </form>';
+            }
+       }
+    }
+  
+}
+
 
 ?>
 
